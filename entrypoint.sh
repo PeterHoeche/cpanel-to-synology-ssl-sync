3b3c29356e45c4ed9551a208a5c90e66605b458c
Peter Hoeche <peter@devproof.de>
2025-01-02 13:55:23 +0100

    Fixes some variable names

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,6 @@
 DOMAIN_ARGS=$(echo $DOMAINS | tr "," " " | xargs -n 1 printf -- '--domain=%s ')
 EXPORT_FOLDER="/usr/src/export"
 
-exec ./cpanel-export-ssl -o $EXPORT_FOLDER -t "$CPANEL_AUTH" -e "$CPANEL_ENDPOINT" $DOMAIN_ARGS
+./cpanel-export-ssl -o $EXPORT_FOLDER -t "$CPANEL_AUTH" -e "$CPANEL_ENDPOINT" $DOMAIN_ARGS
 
-exec ./synology-update-certificate -i "$SYNOLOGY_IP" -p "$SYNOLOGY_PORT" -u "$SYNOLOGY_USER" -w "$SYNOLOGY_PASS" -c $EXPORT_FOLDER  $DOMAIN_ARGS
+./synology-update-certificate -i "$SYNOLOGY_IP" -p "$SYNOLOGY_PORT" -u "$SYNOLOGY_USER" -w "$SYNOLOGY_PASS" -c $EXPORT_FOLDER  $DOMAIN_ARGS
